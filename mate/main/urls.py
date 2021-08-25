4ccd85a31091c16cb9058ec58d1da192fbd42a1e
su0su <lk234869@naver.com>
2021-08-25 10:33:29 +0900

    mo : login, join 완성

--- mate/main/urls.py
+++ mate/main/urls.py
@@ -7,5 +7,8 @@ app_name='main'
 
 urlpatterns = [
     path('',main,name="main"),
-    path('join/',views.join,name='join')
+    path('join/',views.join,name='join'),
+    path('keyword/', views.keyword, name='keyword'),
+    path('success/', views.success, name='success'),
+    path('joinsuccess/', views.joinsuccess, name='joinsuccess'),
 ]
--- mate/main/views.py
+++ mate/main/views.py
@@ -4,9 +4,11 @@ from django.template import loader
 
 # Create your views here.
 def main(request):  
+
     return render(request,'main/login.html')
 
 def join(request):  
+    
     return render(request,'main/join.html')
 
 def keyword(request):
@@ -15,3 +17,15 @@ def keyword(request):
         print(selected)
 
     return render(request,'main/keyword.html')
+
+def success(request):
+    if request.method == 'POST':
+        login = request.POST.getlist('login[]')
+        print(login)
+    return render(request, 'main/success.html')
+
+def joinsuccess(request):
+    if request.method == 'POST':
+        join = request.POST.getlist('join[]')
+        print(join)
+    return render(request, 'main/joinsuccess.html')
